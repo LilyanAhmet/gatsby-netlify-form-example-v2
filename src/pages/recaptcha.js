4c212dc1ab4f661f9903d9c35509778ea186df2f
LilyanAhmet <l.ahmetoglu@qimia.de>
2019-10-22 11:17:11 +0300

    initial

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -3,15 +3,7 @@ import { navigate } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
 import Layout from '../layout'
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
-if (typeof RECAPTCHA_KEY === 'undefined') {
-  throw new Error(`
-  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
-  You probably forget to set it in your Netlify build environment variables. 
-  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
-  Note this demo is specifically for Recaptcha v2
-  `)
-}
+
 
 function encode(data) {
   return Object.keys(data)
@@ -79,7 +71,7 @@ export default function Contact() {
             <textarea name="message" onChange={handleChange} />
           </label>
         </p>
-        <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
+        
         <p>
           <button type="submit">Send</button>
         </p>
